5af3aeab4d1861338725d867eb1da8a61a5a921e
oddmoney <oddmoney@gmail.com>
2019-04-12 09:34:50 +0900

    Updates

---
+++ precache-manifest.4a9bbc8b7ad3aaa509e0acd59666c8f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccf969683f40dcfd0638",
+    "url": "/ymphoto/static/js/runtime~main.1fe6c2fa.js"
+  },
+  {
+    "revision": "b51c42400019cf4947c9",
+    "url": "/ymphoto/static/js/main.19794141.chunk.js"
+  },
+  {
+    "revision": "ffbbbee6b953697bf207",
+    "url": "/ymphoto/static/js/2.1f33cb5f.chunk.js"
+  },
+  {
+    "revision": "b51c42400019cf4947c9",
+    "url": "/ymphoto/static/css/main.1c9ddb54.chunk.css"
+  },
+  {
+    "revision": "66eac9b9a1537b7bd8d284c84631e555",
+    "url": "/ymphoto/index.html"
+  }
+];
