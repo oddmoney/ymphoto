a05731e20e093cc23bd282fc124e95edff441c0a
oddmoney <oddmoney@gmail.com>
2019-04-12 17:09:08 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   }
   componentDidMount() {
     this._getMovies();
+    this._callPhotoApi();
   }
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
@@ -21,17 +22,35 @@ class App extends Component {
   }
   // async 비동기
   _getMovies = async () => {
-    const movies = await this._callApi()
+    const movies = await this._callApi();
     this.setState({
       movies
-    })
+    });    
   }
+  _callPhotoApi = () => {  
+    fetch("http://localhost:8080/rest/photoList.do", {
+      credentials: 'include',  
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json'
+      },      
+      body: {
+       "currentSn": "1",
+       "numPerPage": "10"
+      }
+     })
+     .then(response => response.json())
+     .then(json => console.log(json))
+     .catch(err => console.log(err));
+  }
+
   _callApi = () => {
     return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
     .then(response => response.json())
     //.then(json => console.log(json))
     .then(json => json.data.movies)
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
   }
   render() {
     const { movies } = this.state;
